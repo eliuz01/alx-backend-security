375d3c6858cad1504b5da41148049a2d85a917bc
ELIUZ <osongoeliuz14@gmail.com>
2025-09-06 11:37:42 +0300

    add /static/

--- ip_tracking/admin.py
+++ ip_tracking/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import RequestLog, BlockedIP, SuspiciousIP
 
-# Register your models here.
+
+@admin.register(RequestLog)
+class RequestLogAdmin(admin.ModelAdmin):
+    list_display = ("ip_address", "path", "timestamp", "country", "city")
+    search_fields = ("ip_address", "path")
+    list_filter = ("timestamp", "country", "city")
+
+
+@admin.register(BlockedIP)
+class BlockedIPAdmin(admin.ModelAdmin):
+    list_display = ("ip_address",)
+    search_fields = ("ip_address",)
+
+
+@admin.register(SuspiciousIP)
+class SuspiciousIPAdmin(admin.ModelAdmin):
+    list_display = ("ip_address", "reason", "flagged_at")  
+    search_fields = ("ip_address", "reason")
+    list_filter = ("flagged_at",)  
